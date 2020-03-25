01b7bed2ea9a897bf275b1be22ac8d293e475503
DRVeyl <DRVeyl@live.com>
2020-03-24 20:45:06 -0400

    RO-Decoupler Code Maintenance
    Remove unneeded field declarations
    CurrentEjectionForce doesn't need to be persistent
    Condense start-up into OnStartFinished
    Simplify some methods into part.FindModuleImplementing<>()

--- Source/ROLib/Modules/ModuleRODecoupler.cs
+++ Source/ROLib/Modules/ModuleRODecoupler.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace ROLib
 {
     public class ModuleRODecoupler : PartModule
     {
+        private const string GroupName = "ModuleRODecoupler";
+        private const string GroupDisplayName = "RO-Decoupler";
 
         #region KSPFields
 
@@ -26,18 +24,17 @@ namespace ROLib
         /// <summary>
         /// Displays the current Ejection Force
         /// </summary>
-        [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false, guiName = "Ejection Force:", guiUnits="N", guiFormat="F0", groupName = "ModuleRODecoupler", groupDisplayName = "RO-Decoupler")]
+        [KSPField(guiActiveEditor = true, guiName = "Ejection Force:", guiUnits="N", guiFormat="F0", groupName = GroupName, groupDisplayName = GroupDisplayName)]
         public float currentEjectionForce = 0.0f;
 
         /// <summary>
         /// Operate as a decoupler or a separator.
         /// </summary>
-        [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false, guiName = "Type:", groupName = "ModuleRODecoupler"),
+        [KSPField(isPersistant = true, guiActiveEditor = true, guiName = "Type:", groupName = GroupName),
          UI_Toggle(disabledText = "Decoupler", enabledText = "Separator")]
         public bool isOmniDecoupler = false;
 
         #endregion KSPFields
-
 
         #region Private Variables
 
@@ -46,92 +43,37 @@ namespace ROLib
 
         #endregion Private Variables
 
-
         #region Standard KSP Overrides
-
-        public override void OnStart(StartState state)
-        {
-
-            base.OnStart(state);
-
-            if (!FindDecoupler())
-            {
-                ROLLog.error("Unable to find any Decoupler modules");
-                isEnabled = enabled = false;
-                return;
-            }
-
-            if (HighLogic.LoadedSceneIsFlight)
-            {
-                decouple.isOmniDecoupler = isOmniDecoupler;
-            }
-        }
 
         public override void OnStartFinished(StartState state)
         {
             base.OnStartFinished(state);
-
-            ROLLog.debug("OnStartFinished()...");
-            if (!FindModularPart())
+            decouple = part.FindModuleImplementing<ModuleDecouple>();
+            modularPart = part.FindModuleImplementing<ModuleROTank>();
+            if (!(decouple is ModuleDecouple && modularPart is ModuleROTank))
             {
-                ROLLog.error("Unable to find any Modular Part modules");
+                ROLLog.error($"{part} Unable to find ModuleDecouple or ModuleROTank modules");
                 isEnabled = enabled = false;
                 return;
             }
-
-            SetupUICallbacks();
-
-            if (HighLogic.LoadedSceneIsEditor)
+            else
             {
+                decouple.isOmniDecoupler = isOmniDecoupler;
+                if (modularPart is ModuleROTank)
+                    modularPart.Fields[nameof(modularPart.currentDiameter)].uiControlEditor.onFieldChanged += OnDiameterChange;
                 UpdateImpulseValues();
             }
-
         }
 
         #endregion Standard KSP Overrides
 
-
         #region Custom Methods
 
-        private bool FindDecoupler()
-        {
-            ROLLog.debug("Finding Decoupler...");
-            if (decouple == null)
-                decouple = part.Modules["ModuleDecouple"] as ModuleDecouple;
-            return decouple != null;
-        }
-
-        private ModuleROTank FindModularPart()
-        {
-            if (modularPart is null)
-            {
-                modularPart = part.FindModuleImplementing<ModuleROTank>();
-            }
-            return modularPart;
-        }
-
-        private void SetupUICallbacks()
-        {
-            ROLLog.debug("Setting up UICallbacks...");
-            if (FindModularPart() is ModuleROTank p)
-            {
-                ROLLog.debug("p: " + p);
-                ROLLog.debug("p.Fields[nameof(p.currentDiameter): " + p.Fields[nameof(p.currentDiameter)]);
-                ROLLog.debug("p.Fields: " + p.Fields);
-
-                UI_FloatEdit mp = p.Fields[nameof(p.currentDiameter)].uiControlEditor as UI_FloatEdit;
-                mp.onFieldChanged += new Callback<BaseField, object>(OnDiameterChange);
-            }
-        }
-
         private void OnDiameterChange(BaseField bf, object obj) => UpdateImpulseValues();
-
+        public float EjectionForce => Mathf.Round(Mathf.Min(Mathf.Pow(modularPart.currentDiameter, diamExponent) * 100, maxImpulse));
         public void UpdateImpulseValues()
         {
-            ROLLog.log("UpdateImpulseValues() called");
-            float nf = (float) Math.Min(100f * Math.Pow(modularPart.currentDiameter, diamExponent), maxImpulse);
-            nf = (float) Math.Round(nf, 0);
-            currentEjectionForce = decouple.ejectionForce = nf;
+            currentEjectionForce = decouple.ejectionForce = EjectionForce;
         }
 
         #endregion Custom Methods
