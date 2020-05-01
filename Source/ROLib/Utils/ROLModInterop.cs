55b949dfd957434365c5f2cceeae13e0e6db9e38
DRVeyl <DRVeyl@live.com>
2020-05-01 13:27:21 -0400

    Refactor Reflection Helpers
    Static Assembly/MethodInfo/PropertyInfo/Type vars instead of just bools
    Refresh PartActionResourceEditor sliders instead of dirtying the entire PAW window.
    LINQify LocateAirlock()
    Dead code removal
    Use ROLLog tools

--- Source/ROLib/Utils/ROLModInterop.cs
+++ Source/ROLib/Utils/ROLModInterop.cs
@@ -9,10 +9,10 @@ namespace ROLib
     public static class ROLModInterop
     {
         private static bool initialized = false;
-        private static bool installedFAR = false;
-        private static bool installedRF = false;
-        private static bool installedMFT = false;
-        private static bool installedSolverEngines = false;
+        private static Assembly FARAssembly, RFAssembly, MFTAssembly, SolverEnginesAssembly;
+        private static MethodInfo MFTChangeTotalVolumeMI, MFTCalculateMassMI;
+        private static PropertyInfo SolverEnginesTempPI;
+        private static Type MFTType, SolverEngineType;
 
         public static void UpdateResourceVolume(Part part)
         {
@@ -46,7 +46,6 @@ namespace ROLib
             }
             if (IsFARInstalled())
             {
-                //FARdebug(part);
                 part.SendMessage("GeometryPartModuleRebuildMeshData");
             }
             if (HighLogic.LoadedSceneIsEditor && part.parent==null && part!=EditorLogic.RootPart)//likely the part under the cursor; this fixes problems with modular parts not wanting to attach to stuff
@@ -58,82 +57,45 @@ namespace ROLib
         public static void OnPartTextureUpdated(Part part)
         {
             if (HighLogic.LoadedSceneIsEditor || HighLogic.LoadedSceneIsFlight)
-            {
                 TextureCallbacks.onTextureSetChanged(part);
-            }
         }
 
         private static void PartGeometryUpdate(Part part)
         {
             if (HighLogic.LoadedSceneIsEditor || HighLogic.LoadedSceneIsFlight)
-            {
                 TextureCallbacks.onPartModelChanged(part);
-            }
         }
 
-        private static Transform LocateAirlock(Part part)
-        {
-            foreach (Collider coll in part.GetComponentsInChildren<Collider>())
-            {
-                if (coll.gameObject.tag == "Airlock")
-                {
-                    return coll.transform;
-                }
-            }
-            return null;
-        }
-
-        private static void FARdebug(Part part)
-        {
-            MonoBehaviour.print($"FAR DEBUG FOR PART: {part}");
-            foreach (MeshFilter mf in part.GetComponentsInChildren<MeshFilter>())
-            {
-                MonoBehaviour.print($"FAR debug data || go: {mf.gameObject} || mf: {mf} || mesh: {mf.mesh} || sharedMesh: {mf.sharedMesh}");
-            }
-            MonoBehaviour.print("-------------------------------------------------------------------------");
-        }
+        private static Transform LocateAirlock(Part part) =>
+            part.GetComponentsInChildren<Collider>().FirstOrDefault(x => x.gameObject.tag == "Airlock")?.transform;
 
         public static bool RealFuelsVolumeUpdate(Part part, float liters)
         {
             if (!IsRFInstalled() && !IsMFTInstalled())
             {
-                MonoBehaviour.print($"ERROR: Config for {part} is set to use RF/MFT, but neither RF nor MFT is installed, cannot update part volumes through them.  Please check your configs and/or patches for errors.");
+                ROLLog.error($"Config for {part} is set to use RF/MFT, but neither RF nor MFT is installed, cannot update part volumes through them.  Please check your configs and/or patches for errors.");
                 return false;
             }
-            string targ = IsRFInstalled() ? "RealFuels.Tanks.ModuleFuelTanks,RealFuels" : "RealFuels.Tanks.ModuleFuelTanks,modularFuelTanks";
-            if (Type.GetType(targ) is Type moduleFuelTank && getModuleFuelTanks(part) is PartModule pm)
+
+            if (MFTChangeTotalVolumeMI is MethodInfo && MFTCalculateMassMI is MethodInfo && GetModuleFuelTanks(part) is PartModule pm)
             {
-                MethodInfo mi = moduleFuelTank.GetMethod("ChangeTotalVolume");
                 double volumeLiters = liters;
-                mi.Invoke(pm, new object[] { volumeLiters, false });
-                MethodInfo mi2 = moduleFuelTank.GetMethod("CalculateMass");
-                mi2.Invoke(pm, new object[] { });
+                MFTChangeTotalVolumeMI.Invoke(pm, new object[] { volumeLiters, false });
+                MFTCalculateMassMI.Invoke(pm, new object[] { });
                 UpdatePartResourceDisplay(part);
-                MonoBehaviour.print($"ROTModInterop - Set RF/MFT total tank volume to: {volumeLiters} Liters for part: {part.name}");
+                ROLLog.debug($"ROTModInterop - Set RF/MFT total tank volume to: {volumeLiters} Liters for part: {part.name}");
                 return true;
             } else
             {
-                MonoBehaviour.print($"ERROR! Could not find ModuleFuelTank in part {part} for RealFuels/MFT");
+                ROLLog.error($"Could not find ModuleFuelTank in part {part} for RealFuels/MFT!");
                 return false;
             }
         }
 
-        public static PartModule getModuleFuelTanks(Part part)
-        {
-            string targ = IsRFInstalled() ? "RealFuels.Tanks.ModuleFuelTanks,RealFuels" :
-                          IsMFTInstalled() ? "RealFuels.Tanks.ModuleFuelTanks,modularFuelTanks" :
-                          string.Empty;
-            if (Type.GetType(targ) is Type moduleFuelTank)
-            {
-                return part.GetComponent(moduleFuelTank) as PartModule;
-            } else
-            {
-                MonoBehaviour.print($"ERROR: getModuleFuelTanks for {part} looking for {targ} but failed to find the PartModule.");
-                return null;
-            }
-        }
+        public static PartModule GetModuleFuelTanks(Part part) =>
+            ((IsRFInstalled() || IsMFTInstalled()) && MFTType is Type) ? part.GetComponent(MFTType) as PartModule : null;
 
-        public static bool HasModuleFuelTanks(Part part) => getModuleFuelTanks(part) != null;
+        public static bool HasModuleFuelTanks(Part part) => GetModuleFuelTanks(part) != null;
 
         public static bool hasModuleEngineConfigs(Part part) =>
             Type.GetType("RealFuels.ModuleEngineConfigs,RealFuels") is Type type && part.GetComponent(type) is PartModule;
@@ -141,82 +103,75 @@ namespace ROLib
         public static PropertyInfo getSolverEngineTempProperty()
         {
             if (!IsSolverEnginesInstalled()) return null;
-
-            string targ = "SolverEngines.ModuleEnginesSolver,SolverEngines";
-            if (Type.GetType(targ) is Type t)
-                return t.GetProperty("GetEngineTemp");
-            else
-                return null;
+            return SolverEnginesTempPI;
         }
 
         public static ModuleEngines getSolverEngineModule(Part part, string engineID)
         {
-            if (!IsSolverEnginesInstalled()) return null;
-
-            string targ = "SolverEngines.ModuleEnginesSolver,SolverEngines";
-            if (Type.GetType(targ) is Type moduleSolverEngine)
+            ModuleEngines engine = null;
+            if (IsSolverEnginesInstalled() && SolverEngineType is Type)
             {
-                ModuleEngines engine = Array.ConvertAll(part.GetComponents(moduleSolverEngine), x => (ModuleEngines) x)
-                    .FirstOrDefault(x => x.engineID == engineID);
-
-                engine ??= (ModuleEngines) part.GetComponent(moduleSolverEngine);
-                return engine;
+                engine = Array.ConvertAll(part.GetComponents(SolverEngineType), x => (ModuleEngines) x).FirstOrDefault(x => x.engineID == engineID);
+                engine ??= part.GetComponent(SolverEngineType) as ModuleEngines;
             }
-            else
-            {
-                MonoBehaviour.print($"ERROR: getSolverEngineModule for {part} looking for {targ} but failed to find the PartModule.");
-                return null;
-            }
+            return engine;
         }
 
         private static void Init()
         {
             initialized = true;
-            installedFAR = AssemblyLoader.loadedAssemblies.Any(a => a.assembly.GetName().Name == "FerramAerospaceResearch");
-            installedRF = AssemblyLoader.loadedAssemblies.Any(a => a.assembly.GetName().Name == "RealFuels");
-            installedMFT = AssemblyLoader.loadedAssemblies.Any(a => a.assembly.GetName().Name == "modularFuelTanks");
-            installedSolverEngines = AssemblyLoader.loadedAssemblies.Any(a => a.assembly.GetName().Name == "SolverEngines");
+            FARAssembly = AssemblyLoader.loadedAssemblies.FirstOrDefault(a => a.assembly.GetName().Name == "FerramAerospaceResearch")?.assembly;
+            RFAssembly = AssemblyLoader.loadedAssemblies.FirstOrDefault(a => a.assembly.GetName().Name == "RealFuels")?.assembly;
+            MFTAssembly = AssemblyLoader.loadedAssemblies.FirstOrDefault(a => a.assembly.GetName().Name == "modularFuelTanks")?.assembly;
+            SolverEnginesAssembly = AssemblyLoader.loadedAssemblies.FirstOrDefault(a => a.assembly.GetName().Name == "SolverEngines")?.assembly;
+            if (RFAssembly is Assembly || MFTAssembly is Assembly)
+            {
+                string targ = RFAssembly is Assembly ? "RealFuels.Tanks.ModuleFuelTanks,RealFuels" : "RealFuels.Tanks.ModuleFuelTanks,modularFuelTanks";
+                MFTType = Type.GetType(targ);
+                MFTChangeTotalVolumeMI = MFTType?.GetMethod("ChangeTotalVolume");
+                MFTCalculateMassMI = MFTType?.GetMethod("CalculateMass");
+            }
+            if (SolverEnginesAssembly is Assembly)
+            {
+                SolverEngineType = Type.GetType("SolverEngines.ModuleEnginesSolver,SolverEngines");
+                SolverEnginesTempPI = SolverEngineType?.GetProperty("GetEngineTemp");
+            }
         }
 
         public static bool IsFARInstalled()
         {
             if (!initialized) Init();
-            return installedFAR;
+            return FARAssembly is Assembly;
         }
 
         public static bool IsRFInstalled()
         {
             if (!initialized) Init();
-            return installedRF;
+            return RFAssembly is Assembly;
         }
 
         public static bool IsMFTInstalled()
         {
             if (!initialized) Init();
-            return installedMFT;
+            return MFTAssembly is Assembly;
         }
 
         public static bool IsSolverEnginesInstalled()
         {
             if(!initialized) Init();
-            return installedSolverEngines;
+            return SolverEnginesAssembly is Assembly;
         }
 
         public static void UpdatePartResourceDisplay(Part part)
         {
-            if (HighLogic.LoadedSceneIsEditor && EditorLogic.fetch == null) { return; }
-            if (HighLogic.LoadedSceneIsFlight && FlightDriver.fetch == null) { return; }
-            try
+            if (HighLogic.LoadedSceneIsEditor && part.PartActionWindow is UIPartActionWindow)
             {
-                if (UIPartActionController.Instance != null)
+                foreach (UIPartActionResourceEditor pare in part.PartActionWindow?.ListItems?.Where(x => x is UIPartActionResourceEditor))
                 {
-                    UIPartActionWindow window = UIPartActionController.Instance.GetItem(part);
-                    if (window != null) { window.displayDirty = true; }
+                    pare.resourceMax.text = KSPUtil.LocalizeNumber(pare.Resource.maxAmount, "F1");
+                    pare.UpdateItem();
+                    pare.slider.onValueChanged.Invoke(pare.slider.value);
                 }
-            }
-            catch (Exception e)
-            {
-                MonoBehaviour.print("ERROR: Caught exception while updating part resource display: " + e.Message);
             }
         }
     }
