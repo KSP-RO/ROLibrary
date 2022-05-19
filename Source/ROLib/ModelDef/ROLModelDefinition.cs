f051aefde895e1fca404d427cd73fdd3cb4eac85
Dan <papwr17@gmail.com>
2022-05-19 16:52:16 -0500

    Add modelScale value to set the correct starting size of RCS Parts

--- Source/ROLib/ModelDef/ROLModelDefinition.cs
+++ Source/ROLib/ModelDef/ROLModelDefinition.cs
@@ -629,6 +629,7 @@ namespace ROLib
 
         public readonly Vector3 thrustTransformPositionOffset;
         public readonly float thrustTransformScaleOffset;
+        public readonly float modelScale = 1.0f;
 
         /// <summary>
         /// The thrust of the RCS model at its base scale.
@@ -641,6 +642,8 @@ namespace ROLib
             thrustTransformName = node.GetStringValue(nameof(thrustTransformName));
             thrustTransformPositionOffset = node.GetVector3(nameof(thrustTransformPositionOffset), Vector3.zero);
             thrustTransformScaleOffset = node.GetFloatValue(nameof(thrustTransformScaleOffset), 1f);
+            thrustTransformScaleOffset = node.GetFloatValue(nameof(thrustTransformScaleOffset), 1f);
+            modelScale = node.GetFloatValue(nameof(modelScale), 1f);
             nozzles = node.GetFloatValue(nameof(nozzles), 1);
             enableX = node.GetBoolValue(nameof(enableX), true);
             enableY = node.GetBoolValue(nameof(enableY), true);
