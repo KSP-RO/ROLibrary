44491599d5411876f37d8f21cbdeef57c9687015
ryanc55 <54254625+ryanc55@users.noreply.github.com>
2023-09-23 19:41:01 -0700

    New Class ModuleROPayload (#24)
    Add helper module ModuleROPayload to support solar panel animations and tracking in ROTanks.

---
+++ Source/ROLib/Modules/ModuelROPayload.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ROLib
+{
+    public class ModuleROPayload : ModuleDeployableSolarPanel
+    {
+        protected PartModule pmROTank;
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            pmROTank = part.Modules.GetModule<ModuleROTank>();
+            if (state == StartState.Editor && pmROTank is ModuleROTank moduleROTank)
+            {
+                if (moduleROTank?.Fields["currentCore"] is BaseField cCbf)
+                {
+                    cCbf.uiControlEditor.onFieldChanged += OnCoreChanged;
+                }
+                moduleROTank.enableVScale = false;
+                moduleROTank.Fields[nameof(moduleROTank.currentVScale)].guiActiveEditor = false;
+            }
+        }
+
+        internal void OnCoreChanged(BaseField bf, object obj)
+        {
+            UpdateAnimationAndTracking(bf);
+            startFSM();
+        }
+
+        private void UpdateAnimationAndTracking(BaseField bf)
+        {
+            FindAnimations();
+            panelRotationTransform = part.FindModelTransform(pivotName);
+            hasPivot = panelRotationTransform is Transform;
+            originalRotation = currentRotation = panelRotationTransform?.localRotation ?? Quaternion.identity;
+        }
+
+        private void FindAnimations()
+        {
+            anim = null;
+            if (!string.IsNullOrEmpty(animationName))
+            {
+                Animation[] animations = part.transform.ROLFindRecursive("model").GetComponentsInChildren<Animation>();
+                anim = animations.FirstOrDefault(x => x.GetClip(animationName) is AnimationClip);
+                anim ??= animations.FirstOrDefault();
+            }
+            useAnimation = anim != null;
+        }
+    }
+}
